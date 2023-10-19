6538de8a6077c5b28c1e1791da3df44e1d809bc0
brcshakil1 <brcshakil72@gmail.com>
2023-10-19 11:57:22 +0600

    added middleware and mongodb

--- index.js
+++ index.js
@@ -5,6 +5,42 @@ require("dotenv").config();
 const app = express();
 const port = process.env.PORT || 4000;
 
+// middleware
+app.use(cors());
+app.use(express.json());
+
+// driveWave
+// S7pGkXZhn2tERbYP
+
+const { MongoClient, ServerApiVersion } = require("mongodb");
+const uri =
+  "mongodb+srv://<username>:<password>@cluster0.ufgx0zu.mongodb.net/?retryWrites=true&w=majority";
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
+});
+
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    await client.connect();
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!"
+    );
+  } finally {
+    // Ensures that the client will close when you finish/error
+    await client.close();
+  }
+}
+run().catch(console.dir);
+
 app.get("/", (req, res) => {
   res.send("DriveWave's SERVER SITE IS RUNNING");
 });
