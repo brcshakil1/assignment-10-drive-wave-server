1a57a3a69878fe05a03aaaf13131a56ae4678183
brcshakil1 <brcshakil72@gmail.com>
2023-10-21 22:17:25 +0600

    added

--- index.js
+++ index.js
@@ -29,7 +29,7 @@ const client = new MongoClient(uri, {
 async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
-    await client.connect();
+    // await client.connect();
 
     const allBrandsProductsCollection = client
       .db("allBrandsProductsDB")
